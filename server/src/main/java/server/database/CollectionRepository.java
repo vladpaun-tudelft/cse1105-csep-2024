49e12839a21c12d0f1b49594396495d4f77b396c
Bartosz Bieryło <b.k.bierylo@student.tudelft.nl>
2024-11-20 16:34:55 +0100

    fixed the checkstyle build error in quote repository

--- server/src/main/java/server/database/CollectionRepository.java
+++ server/src/main/java/server/database/CollectionRepository.java
@@ -1,7 +1,6 @@
 package server.database;
 
 import commons.Collection;
-import commons.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CollectionRepository extends JpaRepository<Collection, Long> {
