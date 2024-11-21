2a54a3be42093b96b3e5f395d070f18620db1f33
Vlad Paun <v.a.paun@student.tudelft.nl>
2024-11-21 14:06:16 +0100

    Made a service and 2 controllers to get all notes, or get notes by collection

--- server/src/main/java/server/database/NoteRepository.java
+++ server/src/main/java/server/database/NoteRepository.java
@@ -3,5 +3,8 @@ package server.database;
 import commons.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findByCollectionTitle(String collectionTitle);
 }
