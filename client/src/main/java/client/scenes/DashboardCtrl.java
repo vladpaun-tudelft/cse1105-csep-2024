287a6255532b22c58901f130aea5af6fed81aeee
Koen Draisma <K.G.Draisma@student.tudelft.nl>
2024-11-22 16:39:22 +0100

    Pipeline fail fix Attempt #1: Checkstyle

--- client/src/main/java/client/scenes/DashboardCtrl.java
+++ client/src/main/java/client/scenes/DashboardCtrl.java
@@ -3,14 +3,6 @@ package client.scenes;
 import com.google.inject.Inject;
 
 import client.utils.ServerUtils;
-import commons.Person;
-import commons.Quote;
-import jakarta.ws.rs.WebApplicationException;
-import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.stage.Modality;
 
 /**
  * Controlls all logic for the main dashboard.
